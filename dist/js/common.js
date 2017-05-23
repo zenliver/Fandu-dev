e551c157c3b553c2d2a1263eee3d482c796d9d06
Jerry Zeng <micwind@gmail.com>
2017-05-23 20:50:56 +0800

    继续开发

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,17 +1,11 @@
 // Code by ZenLiver
 // 凡度
 
+// 全局变量
+var screenWidth = $(window).width();
+var screenHeight = $(window).height();
+
 $(function () {
-
-    // 全局变量
-    var screenWidth = $(window).width();
-    var screenHeight = $(window).height();
-
-    // 案例详情页：pad横屏以上自动设置.case_detail_content_section3下的.col-md-4的高度
-    if (screenWidth >= 992) {
-        var caseDetailSection3ImgHeight = $(".case_detail_content_section3 .col-md-8 img").height();
-        $(".case_detail_content_section3 .col-md-4").css("height",caseDetailSection3ImgHeight+"px");
-    }
 
     // 设计师页面：设计师介绍modal
     $(".designer_profile").click(function () {
@@ -28,6 +22,12 @@ $(function () {
         });
     });
 
+    // 案例详情页：pad横屏以上自动设置.case_detail_content_section3下的.col-md-4的高度
+    if (screenWidth >= 992) {
+        var caseDetailSection3ImgHeight = $(".case_detail_content_section3 .col-md-8 img").height();
+        $(".case_detail_content_section3 .col-md-4").css("height",caseDetailSection3ImgHeight+"px");
+    }
+
     // 新闻详情页： .news_detail_section3 区域PC下全屏显示
     if (screenWidth >= 1200) {
         var newsDetailSection3ImgWidth = $(".news_detail_section3 img").width();
@@ -36,5 +36,25 @@ $(function () {
             "margin-right": -(screenWidth-newsDetailSection3ImgWidth)/2+"px"
         });
     }
+
+    // sr动画
+
+        // 启动sr
+        window.sr = ScrollReveal({
+            reset: true,
+            mobile: true,
+            easing: 'ease',
+            distance: '30px'
+        });
+
+        // 添加sr动画
+        // sr.reveal('.case_item .col-md-6:nth-child(1)', {
+        //     duration: 1500,
+        //     origin: 'left'
+        // });
+        // sr.reveal('.case_item .col-md-6:nth-child(2)', {
+        //     duration: 1500,
+        //     origin: 'right'
+        // });
 
 });
